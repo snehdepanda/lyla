b44e8ff791d58d7de9103f3078c52957efdf9831
Sneh Deshpande <snehdeshpande2025@u.northwestern.edu>
2024-05-04 18:35:23 -0500

    server side

--- tornadoserver/camera_server.py
+++ tornadoserver/camera_server.py
@@ -1,20 +1,9 @@
-from api_utils import *
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+from api_utils import *
 
 website_clients = []
-mp3_created = False
-
-class MainHandlerMP3(tornado.web.RequestHandler):
-    def get(self):
-        # if mp3_created is True:
-        # mp3_created = False
-        filepath = "elevenlabs_test.mp3"  # Specify the path to your MP3 file
-        self.set_header('Content-Type', 'audio/mpeg')
-        with open(filepath, "rb") as f:
-            self.write(f.read())
-        self.finish()
 
 class MainHandlerWebsite(tornado.web.RequestHandler):
     def get(self):
@@ -43,18 +32,26 @@ class WebSocketHandlerESP32(tornado.websocket.WebSocketHandler):
     def open(self):
         print("ESP32 WebSocket opened")
         self.api_caller = Caller()
-        website_clients.append(self)
 
     def on_message(self, message):
-        update_all_clients("Message: " + message)
+        self.write_message("ESP32: " + message)
         print("Received message: {}".format(message))
-        #mp3_created = True
+        update_all_clients(message)
+
         # print("Received base 64 image data")
         # update_all_clients(message, bin=False)
         self.api_caller.query(message)
 
     def on_close(self):
         print("ESP32 WebSocket closed")
+
+def make_app():
+    # multiple endpoints
+    return tornado.web.Application([
+        (r"/", MainHandlerWebsite),
+        (r"/websocket", WebSocketHandler),
+        (r"/websocket_esp32", WebSocketHandlerESP32)
+    ])
 
 def update_all_clients(message, bin=False):
     if bin == True:
@@ -65,16 +62,8 @@ def update_all_clients(message, bin=False):
         for client in website_clients:
             client.write_message(message)
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandlerWebsite),
-        (r"/websocket", WebSocketHandler),
-        (r"/websocket_esp32", WebSocketHandlerESP32),
-        (r"/mp3", MainHandlerMP3),
-    ])
-
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
-    print("Server is running on http://localhost:8888/mp3")
+    print("Server is running at http://192.168.4.82:8888")
     tornado.ioloop.IOLoop.current().start()
