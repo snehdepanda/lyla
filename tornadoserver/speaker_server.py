497edb7ddc9a63b4c60f818db4cb5021e9a9c69c
Sneh Deshpande <snehdeshpande2025@u.northwestern.edu>
2024-05-14 20:13:07 -0500

    server

--- tornadoserver/speaker_server.py
+++ tornadoserver/speaker_server.py
@@ -10,66 +10,67 @@ class MainHandlerMP3(tornado.web.RequestHandler):
     def get(self):
         # if mp3_created is True:
         # mp3_created = False
+        print("Sending MP3 file...")
         filepath = "elevenlabs_test.mp3"  # Specify the path to your MP3 file
         self.set_header('Content-Type', 'audio/mpeg')
         with open(filepath, "rb") as f:
             self.write(f.read())
         self.finish()
 
-class MainHandlerWebsite(tornado.web.RequestHandler):
-    def get(self):
-        self.render("index.html")
+# class MainHandlerWebsite(tornado.web.RequestHandler):
+#     def get(self):
+#         self.render("index.html")
 
-# handler for user
-class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        print("WebSocket opened")
-        website_clients.append(self)
+# # handler for user
+# class WebSocketHandler(tornado.websocket.WebSocketHandler):
+#     def open(self):
+#         print("WebSocket opened")
+#         website_clients.append(self)
 
-    def on_message(self, message):
-        # self.write_message("Echo: " + message)
-        update_all_clients("Echo: " + message)
-        print("Received message: {}".format(message))
+#     def on_message(self, message):
+#         # self.write_message("Echo: " + message)
+#         update_all_clients("Echo: " + message)
+#         print("Received message: {}".format(message))
 
-    def on_close(self):
-        print("WebSocket closed")
-        website_clients.remove(self)
+#     def on_close(self):
+#         print("WebSocket closed")
+#         website_clients.remove(self)
 
-# handler for esp32
-class WebSocketHandlerESP32(tornado.websocket.WebSocketHandler):
-    def check_origin(self, origin):
-        return True
+# # handler for esp32
+# class WebSocketHandlerESP32(tornado.websocket.WebSocketHandler):
+#     def check_origin(self, origin):
+#         return True
 
-    def open(self):
-        print("ESP32 WebSocket opened")
-        self.api_caller = Caller()
-        website_clients.append(self)
+#     def open(self):
+#         print("ESP32 WebSocket opened")
+#         self.api_caller = Caller()
+#         website_clients.append(self)
 
-    def on_message(self, message):
-        update_all_clients("Message: " + message)
-        print("Received message: {}".format(message))
-        #mp3_created = True
-        # print("Received base 64 image data")
-        # update_all_clients(message, bin=False)
-        self.api_caller.query(message)
+#     def on_message(self, message):
+#         update_all_clients("Message: " + message)
+#         print("Received message: {}".format(message))
+#         #mp3_created = True
+#         # print("Received base 64 image data")
+#         # update_all_clients(message, bin=False)
+#         self.api_caller.query(message)
 
-    def on_close(self):
-        print("ESP32 WebSocket closed")
+#     def on_close(self):
+#         print("ESP32 WebSocket closed")
 
-def update_all_clients(message, bin=False):
-    if bin == True:
-        for client in website_clients:
-            print('sending data...')
-            client.write_message(message, binary=True)
-    else:
-        for client in website_clients:
-            client.write_message(message)
+# def update_all_clients(message, bin=False):
+#     if bin == True:
+#         for client in website_clients:
+#             print('sending data...')
+#             client.write_message(message, binary=True)
+#     else:
+#         for client in website_clients:
+#             client.write_message(message)
 
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandlerWebsite),
-        (r"/websocket", WebSocketHandler),
-        (r"/websocket_esp32", WebSocketHandlerESP32),
+    #     (r"/", MainHandlerWebsite),
+    #     (r"/websocket", WebSocketHandler),
+    #     (r"/websocket_esp32", WebSocketHandlerESP32),
         (r"/mp3", MainHandlerMP3),
     ])
 
