aec6abed8e2d8d4df90b9cadd0220562d7f5e9c2
Stephen Cheng <stephenscheng111@gmail.com>
2024-05-25 18:37:24 -0500

    init s3 rtos commit

--- lyla/include/ei_utils.h
+++ lyla/include/ei_utils.h
@@ -6,26 +6,7 @@
 
 #define CAMERA_ESP32_S3
 
-#if defined(CAMERA_PICO)
-#define PWDN_GPIO_NUM     21
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      9
-#define SDA_GPIO_NUM      13
-#define SCK_GPIO_NUM      15
-
-#define Y9_GPIO_NUM       20
-#define Y8_GPIO_NUM       10
-#define Y7_GPIO_NUM       11
-#define Y6_GPIO_NUM        7
-#define Y5_GPIO_NUM        5
-#define Y4_GPIO_NUM       22
-#define Y3_GPIO_NUM       19
-#define Y2_GPIO_NUM        8
-#define VSYNC_GPIO_NUM     2
-#define HREF_GPIO_NUM      4
-#define PCLK_GPIO_NUM      6
-
-#elif defined(CAMERA_ESP32_CAM)
+#if defined(CAMERA_ESP32_CAM)
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
